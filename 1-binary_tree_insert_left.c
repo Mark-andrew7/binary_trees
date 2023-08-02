623dbb1f25e59581b7e94b573a725ca1a0a44d6f
Mark-andrew7 <kamaumark19@gmail.com>
2023-08-02 03:18:34 -0700

    Added semi-colon

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *newNode;
 if (parent == NULL)
 return (NULL);
 
-newNode = binary_tree_node(parent, value)
+newNode = binary_tree_node(parent, value);
 if (newNode == NULL)
 return (NULL);
 
