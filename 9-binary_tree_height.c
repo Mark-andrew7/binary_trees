5eb165774714f8dca6555cdef31896221fe587f5
Mark-andrew7 <kamaumark19@gmail.com>
2023-08-03 04:13:30 -0700

    solved compilation error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+
+#define X(a, b) ((a) > (b) ? (a) : (b))
+
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
@@ -13,5 +16,5 @@ return (0);
 
 left_h = binary_tree_height(tree->left);
 right_h = binary_tree_height(tree->right);
-return (1 + MAX(left_h, right_h));
+return (1 + X(left_h, right_h));
 }
